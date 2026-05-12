0d0f23677c6cf800c4bf923a80e3ecaea8977f1c
Your Name <your.email@example.com>
2026-05-12 20:23:31 +0530

    Added hierarchical folder validation

---
+++ V7/helpers/checkFolders.js
@@ -0,0 +1,39 @@
+  import fs from "fs";
+import path from "path";
+
+const checkFolders = ({ inRouteObject }) => {
+
+    if (!inRouteObject) return false;
+
+    const folders = [
+        inRouteObject.root,
+        inRouteObject.version,
+        inRouteObject.tableName,
+        inRouteObject.command
+    ];
+    let currentPath = process.cwd();
+
+    for (let i = 0; i < folders.length; i++) {
+
+        const folderName = folders[i];
+
+        if (!fs.existsSync(path.join(currentPath, folderName))) {
+
+            console.log(
+                `${folderName} folder missing at location ${
+                    currentPath.replace(process.cwd(), "").replaceAll("\\", "/") || "/"
+                }`
+            );
+
+            return false;
+        };
+
+        currentPath = path.join(currentPath, folderName);
+    };
+
+    console.log("Endpoint already exists");
+
+    return true;
+};
+
+export default checkFolders;
